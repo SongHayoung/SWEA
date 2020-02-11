a4624c4fa9fac435fa3bbbeb0f19911d13774fae
SongHayoung <lovelydays95@naver.com>
2020-02-12 00:28:37 +0900

    6min 0sec 최적화 4kb 3ms

---
+++ 8016.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char** argv)
+{
+    int test_case, T;
+    long long n;
+    scanf("%d",&T);
+    for(test_case = 1; test_case <= T; ++test_case){
+        scanf("%ld",&n);
+        printf("#%d %ld %ld\n",test_case,(n-1)*(n-1)<<1^1,(n*n<<1)-1);
+    }
+    return 0;
+}
