da55f582a3342b2964ce281c2bf70ee1cbf092f3
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-02-13 00:09:36 +0900

    3hour 12min 23sec

--- 7193.cpp
+++ 7193.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
-
 using namespace std;
+
 int main(int argc, char** argv)
 {
-    int test_case;
-    int T, N, answer;
-    string s;
-    cin>>T;
-    for (test_case = 1; test_case <= T; ++test_case)
-    {
-        cin>>N>>s;
-        answer = 0;
-        for(int i=0;i<s.length();i++)
-            answer += s[i]-48;
-        
-        cout<<"#"<<test_case<<" "<<(answer%(N-1))<<endl;
-    }
-    return 0; //정상종료시 반드시 0을 리턴해야 합니다.
+	int test_case;
+	int T, N, answer;
+	string s;
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> N >> s;
+		answer = 0;
+		switch (N) {
+		case 2:
+			cout << "#" << test_case << " 0\n";
+			break;
+		default:
+			for (int i = 0; i<s.length(); i++)
+				answer += s[i] - 48;
+			cout << "#" << test_case << " " << (answer % (N - 1)) << endl;
+			break;
+		}
+	}
+	return 0; //정상종료시 반드시 0을 리턴해야 합니다.
 }
