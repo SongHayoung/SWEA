8f2d3c723f7a9fcd4a05be22df2314edeb28d98c
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-02-14 16:47:14 +0900

    5min 0sec

--- 7102.c
+++ 7102.c
@@ -4,7 +4,7 @@
 int main(int argc, char** argv)
 {
     int test_case;
-    int arr[401];
+    int arr[41];
     int T, N, M;
     int max;
     setbuf(stdout, NULL);
@@ -17,15 +17,15 @@ int main(int argc, char** argv)
         for(int i=1;i<=N;i++)
             for(int j=1;j<=M;j++) {
                 arr[i + j]++;
-                max = max > arr[i+j] ? max : arr[i+j];
+                max = max > arr[i + j] ? max : arr[i + j];
             }
         printf("#%d ",test_case);
-        for(int i=1;i<=400;i++){
+        for(int i=2;i<=40;i++){
             if(arr[i]==max)
                 printf("%d ",i);
         }
         printf("\n");
-        
+
     }
     return 0; //정상종료시 반드시 0을 리턴해야 합니다.
 }
