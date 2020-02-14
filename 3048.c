dcc7176392d8cfd714179526b5b0429767114e7d
SongHayoung <lovelydays95@naver.com>
2020-02-14 17:53:14 +0900

    10min 9sec

---
+++ 3048.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char** argv)
+{
+    int test_case, T;
+    long N;
+    setbuf(stdout, NULL);
+    scanf("%d", &T);
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        scanf("%ld",&N);
+        printf("#%d %ld %ld %ld\n",test_case ,N*(N+1)>>1 ,N*N,(N+1)*N);
+    }
+    return 0; //정상종료시 반드시 0을 리턴해야 합니다.
+}
