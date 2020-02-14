9da6fa80846a23966696b18b305287182f9c9617
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-02-14 13:34:45 +0900

    3hour 12min 23sec 최적화 997ms

--- 7193.cpp
+++ 7193.cpp
@@ -1,26 +1,33 @@
-#include <iostream>
-using namespace std;
-
+#include <stdio.h>
 int main(int argc, char** argv)
 {
-	int test_case;
-	int T, N, answer;
-	string s;
-	cin >> T;
-	for (test_case = 1; test_case <= T; ++test_case)
-	{
-		cin >> N >> s;
-		answer = 0;
-		switch (N) {
-		case 2:
-			cout << "#" << test_case << " 0\n";
-			break;
-		default:
-			for (int i = 0; i<s.length(); i++)
-				answer += s[i] - 48;
-			cout << "#" << test_case << " " << (answer % (N - 1)) << endl;
-			break;
-		}
-	}
-	return 0; //정상종료시 반드시 0을 리턴해야 합니다.
+    int test_case;
+    int T, N;
+    long answer;
+    char c;
+    setbuf(stdout, NULL);
+    scanf("%d", &T);
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        scanf("%d",&N);
+        getchar();
+        c='0';
+        switch (N) {
+            case 2:
+                while('0'<=c&&c<='9'){
+                    c = getchar();
+                }
+                printf("#%d 0\n",test_case);
+                break;
+            default:
+                answer = 0;
+                do{
+                    answer += c-48;
+                    c = getchar();
+                }while('0'<=c&&c<='9');
+                printf("#%d %d\n",test_case,answer%(N-1));
+                break;
+        }
+    }
+    return 0; //정상종료시 반드시 0을 리턴해야 합니다.
 }
