c714f0d280f40ea35403bfb99f27ee08840bc97d
SongHayoung <lovelydays95@naver.com>
2020-02-17 15:46:11 +0900

    17min 38sec 최적화 4kb 5ms

--- 7853.c
+++ 7853.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #define MOD 1000000007
 int main(void){
+    register int test_case, idx;
+    register long answer;
+    int T;
+    char c[1001];
+    scanf("%d",&T);
+    getchar();
+    for(test_case = 1; test_case<=T; test_case++){
+        idx = 0;
+        answer = 1;
+        c[idx++] = getchar();
+        c[idx++] = getchar();
+        c[idx++] = getchar();
+        while('a'<=c[idx-1]){
+            answer = c[idx-3]!=c[idx-2]&&c[idx-3]!=c[idx-1]&&c[idx-2]!=c[idx-1] ? (answer+(answer<<1))%MOD : c[idx-3]==c[idx-2]&&c[idx-2]==c[idx-1] ? answer : (answer<<1)%MOD;
+            c[idx++] = getchar();
+        }
+        answer <<= c[0]!=c[1]&&c[idx-2]!=c[idx-3] ? 2 : c[0]==c[1]&&c[idx-2]==c[idx-3] ? 0 : 1;
+        printf("#%d %d\n",test_case,answer%MOD);
+    }
+    return 0;
+}
+/* SAME CODE
+#include <stdio.h>
+#define MOD 1000000007
+int main(void){
     register int test_case, idx, i;
     register long answer;
     int T;
@@ -23,3 +48,4 @@ int main(void){
     }
     return 0;
 }
+*/
