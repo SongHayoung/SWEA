1970015d03534535572ec34dd9caad861bb069f0
SongHayoung <lovelydays95@naver.com>
2020-02-28 17:27:10 +0900

    17min 31sec

--- 4408.c
+++ 4408.c
@@ -27,29 +27,30 @@ int fRI(){
     }while('0'<=N&&N<='9');
     return ret;
 }
+int answer = 0;
 
-void fWA(int tc, int ans){
+void fWA(int tc){
     pc('#');
-    int ret = 0, count = 0, N = tc;
-    while(!(N%10))  {count++; N/=10;}
-    while(N)    {ret = (ret<<3) + (ret<<1) + N%10; N/=10;}
-    while(ret)  {pc(ret % 10 + '0'); ret/=10;}
-    while(count--) pc('0');     pc(' ');
-    N = ans; count = 0;
-    while(!(N%10))  {count++; N/=10;}
-    while(N)    {ret = (ret<<3) + (ret<<1) + N%10; N/=10;}
-    while(ret)  {pc(ret % 10 + '0'); ret/=10;}
-    while(count--) pc('0');     pc('\n');
+    int r = 0, c = 0;
+    while(!(tc%10))  {c++; tc/=10;}
+    while(tc)    {r = (r<<3) + (r<<1) + tc%10; tc/=10;}
+    while(r)  {pc(r % 10 + 48); r/=10;}
+    while(c--) pc(48);     pc(' ');
+    c = 0;
+    while(!(answer%10))  {c++; answer/=10;}
+    while(answer)    {r = (r<<3) + (r<<1) + answer%10; answer/=10;}
+    while(r)  {pc(r % 10 + 48); r/=10;}
+    while(c--) pc(48);     pc('\n');
     return ;
 }
 int main(int argc, char** argv){
-    register int N,f,t,answer,idx,tc, T;
+    register int N,f,t,idx = 0,tc, T, i;
     sINFO info[200];
     T=fRI();
     for(tc=1;tc<=T;tc++) {
         answer = 0;
         N=fRI();
-        for (idx = 0; idx < N; idx++) {
+        for (; idx < N; idx++) {
             f=fRI(); t=fRI();
             f = (f+1)>>1;
             t = (t+1)>>1;
@@ -59,7 +60,7 @@ int main(int argc, char** argv){
         qsort(info,N,sizeof(sINFO),comp);
         while(idx){
             f = 0;
-            for(int i = 0; i < N; i++)
+            for(i = 0; i < N; i++)
                 if(info[i].from>f){
                     f = info[i].to;
                     info[i].from = -1;
@@ -67,7 +68,7 @@ int main(int argc, char** argv){
                 }
             ++answer;
         }
-        fWA(tc,answer);
+        fWA(tc);
     }
     return 0; //정상종료시 반드시 0을 리턴해야 합니다.
 }
