502c4184d90d6c8f07d393630ba837049c80a79a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-02-29 02:07:08 +0900

    1hour 15min 43sec 4kb 3ms MASTER

--- 8189.c
+++ 8189.c
@@ -1,49 +1,44 @@
 #include <stdio.h>
 #define pc(x) putchar(x)
 #define gc() getchar()
-int rev, count, N, _count;
+int rev, N;
+char buf[0x1011] = { ' ', };
 int readINT() {
-	N = gc(), rev = 0;
+	N = gc(), rev = 0x0;
 	do {
 		rev = (rev << 3) + (rev << 1) + (N & 0b1111);    N = gc();
 	} while ('0' <= N && N <= '9');
 	return rev;
 }
 void writeInt(int n, int r) {
-	rev = count = 0;
-	while (!(n % 10)) { count++; n /= 10; } //obtain the count of the number of 0s
-	while (n) { rev = (rev << 3) + (rev << 1) + n % 10; n /= 10; }  //store reverse of N in rev
-	while(r--){
-		N = rev; _count = count;
-		while (N) { pc(N % 10 + '0'); N /= 10; }
-		while (_count--) pc('0');
-		pc(' ');
+	rev = 0x01;
+	do {buf[rev++] = n % 10 + 48; }while(n /= 10); //store reverse of N in rev
+	while (r--) {
+		N = rev;
+		while (N) pc(buf[--N]);
 	}
 }
 int main(int argc, char** argv) {
-	register int T, N, A, B, checked, time, tc, get = 0;
+	register int T, N, A, B, checked, time, tc, get = 0x0;
 	T = readINT();
-	int letter[101], letters[1001] = { 0, };
-	for (tc = 1; tc <= T; tc++) {
+	int letter[101], letters[1001] = { 0x0, };
+	for (tc = 0x1; tc <= T; tc++) {
 		N = readINT(); A = readINT(); B = readINT();
-		pc('#'); writeInt(tc, 1);
-		for (time = 1; time <= N; time++) {
-			letter[time] = readINT();
-			letters[letter[time]] ^= 1;
-		}
-		checked = 1;
-		for (time = 1; time <= letter[N]; time++) {
+		pc('#'); writeInt(tc, checked = 0x1);
+		for (time = 0x1; time <= N; time++) 
+			letters[letter[time] = readINT()] ^= 1;
+		for (time = 0x1; time <= letter[N]; time++) {
 			if (letters[time]) { get++; letters[time] ^= 1; }
 			if (!(get^A) || !((time - letter[checked]) ^ B)) {
-				writeInt(time, (get + 1) >> 1);
+				writeInt(time, (get + 0x1) >> 1);
 				checked += ((get + 1) >> 1);
 				get >>= 1;
 			}
 		}
 		while (get) {
 			if (!(time - letter[checked] ^ B)) {
-				writeInt(time, (get + 1) >> 1);
-				checked += (get + 1) >> 1;
+				writeInt(time, (get + 0x1) >> 1);
+				checked += (get + 0x1) >> 1;
 				get >>= 1;
 			}
 			time++;
