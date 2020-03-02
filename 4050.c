1f5336be95ded8d66b98b4264e0b2c751c78983a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-03-03 00:41:41 +0900

    13min 2sec

--- 4050.c
+++ 4050.c
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <memory.h>
 #include <stdlib.h>
+
+int clothes[100000], N;
+long answer = 0;
+
 int readINT() {
 	int N = gc(), ret = 0;
 	do {
@@ -11,7 +15,7 @@ int readINT() {
 	} while ('0' <= N);
 	return ret;
 }
-void writeANSWER(int tc, long n) {
+void writeANSWER(int tc) {
 	long rev = 0, count = 0;
 	pc('#');
 	while (!(tc % 10)) { count++; tc /= 10; } 
@@ -19,8 +23,8 @@ void writeANSWER(int tc, long n) {
 	while (rev) { pc(rev % 10 | 0b110000); rev /= 10; }
 	while (count--) pc('0');
 	count = 0;	pc(' ');
-	while (!(n % 10)) { count++; n /= 10; } 
-	while (n) { rev = (rev << 3) + (rev << 1) + n % 10; n /= 10; }  
+	while (!(answer % 10)) { count++; answer /= 10; }
+	while (answer) { rev = (rev << 3) + (rev << 1) + answer % 10; answer /= 10; }
 	while (rev) { pc(rev % 10 | 0b110000); rev /= 10; }
 	while (count--) pc('0');
 	pc('\n');
@@ -29,16 +33,13 @@ void writeANSWER(int tc, long n) {
 int compare(const void *a, const void *b) {
 	return *(int*)a > *(int*)b ? 1 : -1;
 }
-int clothes[100000], N;
-long answer;
 int main(int argc, char** argv) {
 	int tc, T = readINT();
 	for (tc = 1; tc <= T; tc++) {
 		N = readINT();
-		answer = 0;
 		for (int i = 0; i < N; i++) clothes[i] = readINT();
 		qsort(clothes, N, sizeof(int), compare);
 		for (int i = N - 1; i >= 0; i--) if ((N - i) % 3) answer += clothes[i];
-		writeANSWER(tc, answer);
+		writeANSWER(tc);
 	}
 }
