9eec3388383337b50f1566bb317fb0e87d070123
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-03-03 01:02:51 +0900

    3hour 3min 5sec

--- 1824.cpp
+++ 1824.cpp
@@ -17,16 +17,18 @@ int readINT() {
 	int N = gc(), ret = 0;
 	for (; N<'0' || N>'9'; N = gc());
 	do {
-		ret = (ret << 3) + (ret << 1) + N - '0';    N = gc();
-	} while ('0' <= N && N <= '9');
+		ret = (ret << 3) + (ret << 1) + (N&0b1111);    N = gc();
+	} while ('0' <= N );
 	return ret;
 }
 void writeInt(int n) {
 	int rev = 0, count = 0;
+	pc(0x23);
 	while (!(n % 10)) { count++; n /= 10; } //obtain the count of the number of 0s
 	while (n) { rev = (rev << 3) + (rev << 1) + n % 10; n /= 10; }  //store reverse of N in rev
-	while (rev) { pc(rev % 10 + '0'); rev /= 10; }
-	while (count--) pc('0');
+	while (rev) { pc(rev % 10 |0b110000); rev /= 10; }
+	while (count--) pc(0x30);
+	pc(0x20);
 	return;
 }
 char cmd[20][21];
@@ -35,10 +37,10 @@ int dx[4] = { 0,1,0,-1 };
 int dy[4] = { -1,0,1,0 };
 int INFO[20][20][4][16];
 queue<curser> q;
-bool simulation() {
+void simulation() {
 	memset(INFO, 0, sizeof(INFO));
 	INFO[0][0][1][0] = 1;
-	if(!q.empty) q = queue<curser>();
+	if (!q.empty()) q = queue<curser>();
 	q.emplace(0, 0, 1, 0);
 	while (!q.empty()) {
 		int x = q.front().x, y = q.front().y, dir = q.front().dir, mem = q.front().mem;
@@ -59,7 +61,7 @@ bool simulation() {
 		case '_': dir = mem ? 3 : 1; break;
 		case '|': dir = mem ? 0 : 2; break;
 		case '.': break;
-		case '@': return true;
+		case '@': pc('Y'); pc('E'); pc('S'); pc(0x0A); return;
 		case '>': dir = 1; break;
 		case '<': dir = 3; break;
 		case 'v': dir = 2; break;
@@ -75,16 +77,15 @@ bool simulation() {
 		y = !(y + dy[dir] ^ -1) ? R - 1 : !(y + dy[dir] ^ R) ? 0 : y + dy[dir];
 		if (!(INFO[y][x][dir][mem]++)) q.emplace(x, y, dir, mem);
 	}
-	return false;
+	pc('N'); pc('O'); pc(0x0A);
+	return ;
 }
 int main(int argc, char** argv) {
 	int tc, T = readINT();
 	for (tc = 1; tc <= T; tc++) {
 		R = readINT(), C = readINT();
+		writeInt(tc);
 		for (int i = 0; i < R; i++) scanf("%s", cmd[i]);
-		pc('#');  writeInt(tc); pc(' ');
-		if (simulation()) { pc('Y'), pc('E'), pc('S'); }
-		else { pc('N'), pc('O'); }
-		pc('\n');
+		simulation();
 	}
 }
