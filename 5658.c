0cbc3678ef14130ed2eb5081137ada79fb8664e7
SongHayoung <lovelydays95@naver.com>
2020-03-10 14:11:01 +0900

    43min 2sec

--- 5658.c
+++ 5658.c
@@ -3,7 +3,6 @@
 #define gc() getchar_unlocked()
 #define pc(x) putchar_unlocked(x)
 #define ADD3(a,b) ((a<<3)+(a<<1)+(b))
-#define ADD4(a,b,c,d) ((a)+(b)*(c)+(d))
 #define REP(i,a,b) for(i = a; i < b; i++)
 char buf[35];
 int fRI(){
@@ -15,9 +14,9 @@ int fRI(){
     return r;
 }
 
-void fWL(long long tc) {
+void fWL(long tc) {
     if(!tc) {pc(0x30); return;}
-    long long r = 0, c = 0;
+    long r = 0, c = 0;
     while (!(tc % 10)) { c++; tc /= 10; }
     while (tc) { r = ADD3(r,tc%10); tc /= 10; }
     while (r) { pc(r % 10 + 48); r /= 10; }
@@ -25,46 +24,40 @@ void fWL(long long tc) {
     return;
 }
 int compare(const void* A, const void* B){
-    return *(long long*)A > *(long long*)B ? -1 : 1;
+    return *(long*)A > *(long*)B ? -1 : 1;
 }
-long long getData(char _buf[7], int N){
-    long long ret = 0, pow = 1;
+long getData(char *_buf, int N){
+    long ret = 0, pow = 1;
     while(N--){
-        switch(_buf[N]){
+        switch(*(_buf+N)){
             case 'A' : ret = pow*10 + ret; break;
             case 'B' : ret = pow*11 + ret; break;
             case 'C' : ret = pow*12 + ret; break;
             case 'D' : ret = pow*13 + ret; break;
             case 'E' : ret = pow*14 + ret; break;
             case 'F' : ret = pow*15 + ret; break;
-            default  : ret = pow*(_buf[N]&0b1111) + ret; break;
+            default  : ret = pow*(*(_buf+N)&0b1111) + ret; break;
         }
         pow *= 16;
     }
     return ret;
 }
 int main(void){
-    register int tc = 1, T=fRI(), N, K, datalen, _K, i, j, k;
-    long long DATA[28];
-    char _buf[7];
+    register int tc = 1, T=fRI(), N, K, datalen, i;
+    long DATA[28];
     while(T--){
-        N=fRI(); K=fRI();
+        N=fRI(); K=fRI(); datalen = 0;
         REP(i,0,N) buf[i] = gc();
         REP(i,0,N>>2) buf[N+i] = buf[i];
-        datalen = 0;
-        REP(i,0,N>>2)
-        REP(j,0,4){
-            REP(k,0,N>>2) _buf[k] = buf[ADD4(i,j,N>>2,k)];
-            DATA[datalen++] = getData(_buf,N>>2);
-        }
-        qsort(DATA,datalen,sizeof(long long),compare);
+        REP(i,0,N)  DATA[datalen++] = getData((buf+i),N>>2);
+        qsort(DATA,datalen,sizeof(long),compare);
         pc(0x23); fWL(tc++); pc(0x20);
         if(!(K^1)) fWL(DATA[0]);
         else{
-            _K = 2;
+            N = 2;
             REP(i,1,datalen){
-                if(!(_K^K)) {fWL(DATA[i]); break;}
-                if(DATA[i]^DATA[i-1]) _K++;
+                if(!(N^K)) {fWL(DATA[i]); break;}
+                if(DATA[i]^DATA[i-1]) N++;
             }
         }pc('\n');
     }
